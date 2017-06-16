47fa447a3b178f264ecefc86b6045f4c7e62ef8b
Philipp Krenn <pk@xeraa.net>
2017-06-16 02:50:25 +0200

    CHANGE frontend cleanup

--- java/frontend/src/main/java/net/xeraa/frontend/FrontendApplication.java
+++ java/frontend/src/main/java/net/xeraa/frontend/FrontendApplication.java
@@ -1,11 +1,13 @@
-package net.xeraa;
+package net.xeraa.frontend;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,9 +17,9 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
-public class MicroserviceMonitoringApplication {
+public class FrontendApplication {
 
-	private static final Logger log = Logger.getLogger(MicroserviceMonitoringApplication.class.getName());
+	private static final Logger log = Logger.getLogger(FrontendApplication.class.getName());
 
 	@Autowired
 	private RestTemplate restTemplate;
@@ -31,19 +33,14 @@ public class MicroserviceMonitoringApplication {
 		return new RestTemplate();
 	}
 
-	// How often to sample with Zipkin
-	@Bean
-	public AlwaysSampler defaultSampler() {
-		return new AlwaysSampler();
-	}
-
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceMonitoringApplication.class, args);
+		SpringApplication.run(FrontendApplication.class, args);
 	}
 
 	@RequestMapping("/good")
-	public String good() {
-		return "OK";
+	public String good(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
 	}
 
 	@RequestMapping("/bad")
@@ -55,13 +52,7 @@ public class MicroserviceMonitoringApplication {
 	public String callHome() throws InterruptedException {
 		log.log(Level.INFO, "Calling home");
 		Thread.sleep(this.random.nextInt(2000));
-		return restTemplate.getForObject("http://frontend.xeraa.wtf:80/home", String.class);
-	}
-
-	@RequestMapping("/home")
-	public String home() {
-		log.log(Level.INFO, "You called home");
-		return "Hello world";
+		return restTemplate.getForObject("http://backend.xeraa.wtf:80/home", String.class);
 	}
 
 	@RequestMapping("/call-bad")
