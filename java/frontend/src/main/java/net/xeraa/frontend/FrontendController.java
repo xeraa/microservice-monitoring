d1e3aa1e9a859637ceffaa8c54f8f020a9e3fec8
Philipp Krenn <pk@xeraa.net>
2017-10-07 02:45:35 +0200

    ADD MDC logging and make the frontend app URLs configurable

--- java/frontend/src/main/java/net/xeraa/frontend/FrontendController.java
+++ java/frontend/src/main/java/net/xeraa/frontend/FrontendController.java
@@ -1,13 +1,17 @@
 package net.xeraa.frontend;
 
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,36 +27,50 @@ public class FrontendController {
 	@Autowired
 	private Random random;
 
+	@Value("${APP_BACKEND:#{'http://localhost:8081'}}")
+	private String backendUrl;
+
+	@Value("${APP_FRONTEND:#{'http://localhost:8080'}}")
+	private String frontendUrl;
+
 	@RequestMapping("/good")
 	public String good(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
+		if(!"World".equals(name)) {
+			MDC.put("name", name);
+		}
+		log.log(Level.INFO, "Calling something good");
 		return "good";
 	}
 
 	@RequestMapping("/bad")
 	public String bad() {
+		log.log(Level.INFO, "Calling something bad");
 		throw new RuntimeException("My bad, something went wrong...");
 	}
 
 	@RequestMapping("/call")
 	public String callHome() throws InterruptedException {
-		log.log(Level.INFO, "Calling home");
+		String callUrl = frontendUrl + "/home";
+		log.log(Level.INFO, "Calling " + callUrl);
 		Thread.sleep(this.random.nextInt(2000));
-		return restTemplate.getForObject("https://backend.xeraa.wtf/home", String.class);
+		return restTemplate.getForObject(callUrl, String.class);
 	}
 
 	@RequestMapping("/call-bad")
 	public String callBad() throws InterruptedException {
-		log.log(Level.INFO, "Calling bad");
+		String callUrl = frontendUrl + "/bad";
+		log.log(Level.INFO, "Calling " + callUrl);
 		Thread.sleep(this.random.nextInt(2000));
-		return restTemplate.getForObject("https://frontend.xeraa.wtf/bad", String.class);
+		return restTemplate.getForObject(callUrl, String.class);
 	}
 
 	@RequestMapping("/call-nested")
 	public String callNested() throws InterruptedException {
-		log.log(Level.INFO, "Calling call");
+		String callUrl = frontendUrl + "/call";
+		log.log(Level.INFO, "Calling " + callUrl);
 		Thread.sleep(this.random.nextInt(1000));
-		return restTemplate.getForObject("https://frontend.xeraa.wtf/call", String.class);
+		return restTemplate.getForObject(callUrl, String.class);
 	}
 
 }
