dccc894cfe22e03404dd0616a644c1f68fbe2325
Philipp Krenn <pk@xeraa.net>
2018-09-10 10:56:45 +0200

    CHANGE kick out security since it was only adding unnecessary complexity

--- java/backend/src/main/java/net/xeraa/backend/BackendApplication.java
+++ java/backend/src/main/java/net/xeraa/backend/BackendApplication.java
@@ -1,35 +1,20 @@
 package net.xeraa.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class BackendApplication extends WebSecurityConfigurerAdapter {
+public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	// Inject the required headers to keep track of requests
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.authorizeRequests()
-				.requestMatchers(EndpointRequest.to("health")).permitAll()
-				.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
-				.antMatchers("/**").permitAll()
-				.and()
-				.httpBasic();
 	}
 
 }
